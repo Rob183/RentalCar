617b6b9a3ff859a93e8fa13acdfbb75cc060a312
Robert <robert_sei@gmx.de>
2023-02-26 23:32:23 +0100

    Model View

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +10,16 @@ namespace RentalCar.Models
     internal class Customer
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
         public int AmountBookings { get; set; }
-        public bool RentalStatus { get; set; }
+
+        public Customer(int id)
+        {
+            Id = id;
+            AmountBookings = 0;
+        }
+        public override string ToString() => $"Id: {Id}; Vorname: {FirstName}; Nachname: {LastName}; Adresse: {Address}";
     }
 }
